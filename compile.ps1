7ddef83c77beb0525facbbd570805d6325f00eef
proydoha <44492748+proydoha@users.noreply.github.com>
2024-01-12 05:59:27 +0200

    Replace Write-Host empty strings with proper new lines

--- compile.ps1
+++ compile.ps1
@@ -8,8 +8,7 @@ $savePath = Join-Path $currentFolder $compiledFolderName
 if (-not (Test-Path -Path $savePath -PathType Container)) {
     Write-Host "Creating ""$compiledFolderName"" folder..."
     New-Item -ItemType Directory -Path $savePath -Force
-    Write-Host "Done!"
-    Write-Host ""
+    Write-Host "Done!`n"
 }
 
 $finalSavePath = Join-Path $savePath $archiveName
@@ -21,29 +20,24 @@ $itemsToArchive = Get-ChildItem -Path $folderPath | Where-Object { $_.Name -noti
 if (Test-Path $finalSavePath) {
     Write-Host "Removing old .zip file..."
     Remove-Item $finalSavePath -Force
-    Write-Host "Done!"
-    Write-Host ""
+    Write-Host "Done!`n"
 }
 if (Test-Path $pk3SavePath) {
     Write-Host "Removing old .pk3 file..."
     Remove-Item $pk3SavePath -Force
-    Write-Host "Done!"
-    Write-Host ""
+    Write-Host "Done!`n"
 }
 
 Write-Host "Archiving..."
 Compress-Archive -Path $itemsToArchive.FullName -DestinationPath $finalSavePath -CompressionLevel 'NoCompression'
-Write-Host "Done!"
-Write-Host ""
+Write-Host "Done!`n"
 
 Write-Host "Renaming to .pk3..."
 Rename-Item -Path $finalSavePath -NewName $pk3Name
-Write-Host "Done!"
-Write-Host ""
+Write-Host "Done!`n"
 
 Write-Host ".pk3 file created at: "
-Write-Host $pk3SavePath
-Write-Host ""
+Write-Host "$pk3SavePath`n"
 
 Write-Host "Press any key to close this powershell window..."
 $key = [System.Console]::ReadKey()
